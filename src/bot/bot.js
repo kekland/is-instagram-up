e8941df4492bec776494b5bfb738014313dee208
kekland <kk.erzhan@gmail.com>
2019-06-10 23:26:08 +0600

    Starting to work on the bot

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,4 +1,13 @@
-const bot = require('node-vk-bot-api')
+const VK = require('vk-fast-longpoll');
+const handler = require('./handler')
 
-const token = require("../data/token.json").token
+const token = require('../../data/token.json').token
+const bot = new VK(token);
 
+bot.longpoll.start();
+
+bot.longpoll.on('message', (message) => {
+  if (!message.isOutbox) {
+    handler.handleMessage(bot, message)
+  }
+});
---
+++ src/bot/handler.js
@@ -0,0 +1,10 @@
+const responses = require('./responses.json')
+
+const handleMessage = async (bot, message) => {
+  console.log(message)
+  if(message == 'Start') {
+
+  }
+}
+
+module.exports.handleMessage = handleMessage
