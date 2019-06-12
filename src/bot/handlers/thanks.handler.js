542db41b1e743900268786864d7a82dde6d9fc1d
kekland <kk.erzhan@gmail.com>
2019-06-12 20:53:50 +0600

    bugfix

--- src/bot/handlers/thanks.handler.js
+++ src/bot/handlers/thanks.handler.js
@@ -3,7 +3,7 @@ const keyboards = require('./keyboards.json')
 
 const tryHandle = async (bot, message, firebase, cachedStatus, users, onSubscribe, onUnsubscribe, logger) => {
   const text = message.text.toLowerCase()
-  if (text.contains('thanks') || text.contains('спасибо') || text.contains('спс') || text.contains('спасиб')) {
+  if (text.includes('thanks') || text.includes('спасибо') || text.includes('спс') || text.includes('спасиб')) {
     bot.api.messages.send({ user_id: message.sender, message: responses.thanks })
     logger.logResponse('unknown')
   }
