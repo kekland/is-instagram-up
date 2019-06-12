e3da612c55f0d46517644adb6fb3483e05aa4b36
kekland <kk.erzhan@gmail.com>
2019-06-12 22:36:50 +0600

    added keyboard event on status handler

--- src/bot/handlers/status.handler.js
+++ src/bot/handlers/status.handler.js
@@ -4,7 +4,7 @@ const keyboards = require('./keyboards.json')
 const tryHandle = async (bot, message, firebase, cachedStatus, users, onSubscribe, onUnsubscribe, logger) => {
   const text = message.text.toLowerCase()
   if (text === '/status' || text === 'статус сервисов' || text === 'статус' || text === 'status') {
-    bot.api.messages.send({ user_id: message.sender, message: cachedStatus })
+    bot.api.messages.send({ user_id: message.sender, message: cachedStatus, keyboard: keyboards.subscribedKeyboard  })
     logger.logResponse('status')
     return true
   }
